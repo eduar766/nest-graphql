04ed07df39e41f6ea365e034ff01cbbab2128d9c
Eduardo Saavedra <eduar766@gmail.com>
2022-04-09 14:00:48 -0400

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver
+    }),
+    ItemsModule,
+    MongooseModule.forRoot('mongodb://localhost/nestgraphql')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,4 +1,45 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { ItemType } from './dto/create-item.dto';
+import { ItemInput } from './input-items.input';
+import { Item } from './interfaces/item.interface';
+import { ItemsService } from './items.service';
 
-@Resolver()
-export class ItemsResolver {}
+@Resolver(of => ItemType)
+export class ItemsResolver {
+    constructor(
+        private readonly itemsService: ItemsService
+    ) {};
+
+    @Query(returns => [ItemType])
+    async items(): Promise<ItemType[]> {
+        return this.itemsService.findAll();
+    }
+
+    @Mutation(returns => ItemType)
+    async createItem(
+        @Args('input') input: ItemInput
+    ): Promise<ItemType> {
+        return this.itemsService.create(input);
+    }
+
+    @Mutation(returns => ItemType)
+    async updateItem(
+        @Args('id') id: string,
+        @Args('input') input: ItemInput
+    ) {
+        return this.itemsService.update(id, input as Item);
+    }
+
+    @Mutation(returns => ItemType)
+    async deleteItem(
+        @Args('id') id: string
+    ) {
+        return this.itemsService.delete(id);
+    }
+
+    @Query(returns => String)
+    async hello() {
+        return 'hello';
+    }
+
+}
